711f98cb5511a0abd544fb0a2b6b5f947c2c4870
Maghish <mail4mithran@gmail.com>
2024-02-19 23:26:44 +0530

    Demo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,7 @@ import "../css/index.css";
 function App() {
   return (
     <>
+      <p className="text-7xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 inline-block text-transparent bg-clip-text">Hello World! Welcome to Rate My Tech Stack!!</p>
     </>
   );
 }
