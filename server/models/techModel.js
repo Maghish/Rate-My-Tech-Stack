3f3edc58641e074b56397e1a8fad44928e84b8b7
Maghish <mail4mithran@gmail.com>
2024-02-20 00:09:29 +0530

    Created Tech stack

---
+++ server/models/techModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const techSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    ratings: { type: Number, required: true },
+    icon: { type: String, required: true },
+    field: {
+      type: String,
+      required: true,
+      enum: [
+        "language",
+        "frontend-framework",
+        "frontend-library",
+        "backend-framework",
+        "backend-library",
+        "api",
+        "database",
+        "style",
+        "cloud-services",
+        "deployment"
+      ],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Tech", techSchema);
