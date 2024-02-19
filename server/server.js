100b34817fa0d818eeea7e30bb0e5accefe7acbb
Maghish <mail4mithran@gmail.com>
2024-02-19 22:45:45 +0530

    Connected MongoDB to the backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+dotenv.config();
 const app = express();
 
-app.listen(4000, () => {
-  console.log('listening on port 4000');
-})
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("MongoDB Connected Successfully");
+  app.listen(4000, () => {
+    console.log(`listening on port ${process.env.PORT}`);
+  });
+});
