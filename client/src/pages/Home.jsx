03d68f0e0a00f4e25ae3243dc5f627180df9feac
Maghish <mail4mithran@gmail.com>
2024-02-20 12:42:33 +0530

    Testing overall rating

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [item1, setItem1] = useState(1);
+  const [item2, setItem2] = useState(2);
+  const [item3, setItem3] = useState(3);
+  const [item4, setItem4] = useState(10);
+  const [overallRating, setOverallRating] = useState(0);
+
+  const checkOverallRating = () => {
+    let rating = (((item1/10) + (item2/10) + (item3/10) + (item4/10)) / 4) * 10
+    setOverallRating(rating.toFixed(1));
+  }
+
+  useEffect(() => checkOverallRating, [])
+
   return (
     <>
-      <p className="text-black text-9xl font-mono">Home</p>
+      <p className="text-6xl">{item1}/10</p>
+      <p className="text-6xl">{item2}/10</p>
+      <p className="text-6xl">{item3}/10</p>
+      <p className="text-6xl">{item4}/10</p>
+      <p className="text-9xl">{overallRating}/10</p>
     </>
   );
 }
